28cfe1793ac3cdeb18fb2646f42579931c8b8b2c
azavoloko <azavoloko@seatgeek.com>
2021-08-13 19:51:46 +0300

    my change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import CategoriesSort from "./components/CategoriesSort/CategoriesSort";
+import { Grid } from "@material-ui/core";
 import Header from "./components/Header";
 import Products from "./components/Products";
+import React from "react";
 import productsList from "./products";
 import { useState } from "react";
 
@@ -9,10 +10,14 @@ const App = () => {
   const [filteredProducts, setFilteredProducts] = useState(products);
 
   return (
-    <div>
-      <Header products={products} setProducts={setFilteredProducts} />
-      <Products products={filteredProducts} />
-    </div>
+    <Grid container direction="column">
+      <Grid item> this is where the header will be </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={12} sm={8} />
+      </Grid>
+      <Grid item xs={0} sm={2} />
+    </Grid>
   );
 };
 
